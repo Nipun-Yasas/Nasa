25c88c391c6eea617ea07695453b917c13a24afc
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-02-12 00:37:29 +0530

    added footer,main,sidebar components

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <di className='imagecontainer'>
+      <img src="mars.png" alt="picture of mars" className='bgimage'/>
+    </di>
+  )
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div>
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+            <p>Description</p>
+            <p>dfddddd</p>
+        </div>
+    </div>
+  )
+}
